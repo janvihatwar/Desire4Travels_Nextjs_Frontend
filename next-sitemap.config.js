d40e9415a17b37a28560a40df821ae36e71a1b29
rohit-pratap-singh-code <rohit.pratap.singh.engg@gmail.com>
2025-06-08 17:58:16 +0530

    Update sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'http://localhost:3000',
+  siteUrl: 'https://desire4travels-nextjs-frontend.vercel.app',
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: 'weekly',
