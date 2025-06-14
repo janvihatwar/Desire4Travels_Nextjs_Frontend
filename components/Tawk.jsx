33b1f4537e13bb3632f639f250c2fcb5c5ba5b9a
rohit-pratap-singh-code <rohit.pratap.singh.engg@gmail.com>
2025-06-14 14:25:04 +0530

    Largest Contentful image

--- components/Tawk.jsx
+++ components/Tawk.jsx
@@ -2,23 +2,28 @@ import React, { useEffect } from 'react';
 import styles from './Tawk.module.css';
 
 const Tawk = () => {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.async = true;
-        script.src = 'https://embed.tawk.to/5c59ccd27cf662208c942f18/default';
-        script.charset = 'UTF-8';
-        script.setAttribute('crossorigin', '*');
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
 
-        document.body.appendChild(script);
+    script.src = 'https://embed.tawk.to/5c59ccd27cf662208c942f18/1djj7vfs3';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
 
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
+    document.body.appendChild(script);
 
-    return (
-        <div className={styles['tawk-custom-container']}></div>
-    );
+    script.onerror = () => {
+      console.error('Failed to load Tawk.to script.');
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className={styles['tawk-custom-container']} aria-hidden="true" />
+  );
 };
 
 export default Tawk;
